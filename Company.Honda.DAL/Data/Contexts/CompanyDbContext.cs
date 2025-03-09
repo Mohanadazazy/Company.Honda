ab94f3c03467d611cb6b5908235d85b279b0d563
Mohanadazazy <mohanadazazy@gmail.com>
2025-03-09 20:52:07 +0200

    Implementation Of DaL

---
+++ Company.Honda.DAL/Data/Contexts/CompanyDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Company.Honda.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Company.Honda.DAL.Data.Contexts
+{
+    public class CompanyDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = .; Database = CompanyPr; Trusted_Connection = True; TrustServerCertificate = True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        DbSet<Department> Department { get; set; } 
+    }
+}
