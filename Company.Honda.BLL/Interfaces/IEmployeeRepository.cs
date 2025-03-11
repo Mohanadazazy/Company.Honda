bdb661d6d9b90d91928ed7cd8abac7c1d687279a
Mohanadazazy <mohanadazazy@gmail.com>
2025-03-11 16:04:55 +0200

    Implementation Of EmployeeModuleBLL

---
+++ Company.Honda.BLL/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Company.Honda.DAL.Models;
+
+namespace Company.Honda.BLL.Interfaces
+{
+    public interface IEmployeeRepository
+    {
+        public IEnumerable<Employee> GetAll();
+        public Employee? Get(int id);
+        public int Add(Employee employee);
+        public int Update(Employee employee);
+        public int Delete(Employee employee);
+    }
+}
---
+++ Company.Honda.BLL/Repositories/EmployeeRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Company.Honda.BLL.Interfaces;
+using Company.Honda.DAL.Data.Contexts;
+using Company.Honda.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Company.Honda.BLL.Repositories
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly CompanyDbContext _context;
+        public EmployeeRepository(CompanyDbContext dbContext)
+        {
+            _context = dbContext;
+        }
+        public Employee? Get(int id)
+        {
+            return _context.Employees.Find(id);
+        }
+
+        public IEnumerable<Employee> GetAll()
+        {
+            return _context.Employees.ToList();
+        }
+
+        public int Add(Employee employee)
+        {
+            _context.Employees.Add(employee);
+            return _context.SaveChanges();
+        }
+        public int Update(Employee employee)
+        {
+            _context.Employees.Update(employee);
+            return _context.SaveChanges();
+        }
+
+        public int Delete(Employee employee)
+        {
+            _context.Employees.Remove(employee);
+            return _context.SaveChanges();
+        }
+
+        
+
+        
+    }
+}
