9d0ceb1752e98d54f54ec42676996641946eadda
Mohanadazazy <mohanadazazy@gmail.com>
2025-03-11 23:30:54 +0200

    Implementation Of EmployeeModuleDetailsAndUpdate(Action-View)

--- Company.Honda.PL/Dtos/EmployeeDto.cs
+++ Company.Honda.PL/Dtos/EmployeeDto.cs
@@ -9,7 +9,7 @@ namespace Company.Honda.PL.Dtos
         public int? Age { get; set; }
         [EmailAddress(ErrorMessage ="example@gmil.com")]
         public string Email { get; set; }
-        [RegularExpression("[09]{0,3}-[a-zA-Z]{0,4}-[a-zA-Z]{0,3}-[a-zA-Z]{0,3}$")]
+        [RegularExpression("[0-9]{0,3}-[a-zA-Z]{0,4}-[a-zA-Z]{0,3}-[a-zA-Z]{0,3}$")]
         public string Address { get; set; }
         [RegularExpression("01[0125][0-9]{8}$")]
         public string Phone { get; set; }
